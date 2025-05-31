9675d000ce58e126eace467c89efd8713f7deae6
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-05-31 10:36:13 -0300

    Elementos do Login

--- client/src/pages/DaySelection.tsx
+++ client/src/pages/DaySelection.tsx
@@ -1,47 +1,43 @@
-// // PAGINA 1
+import './DaySelection.css';
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import Logo from '../image/upscalemedia-transformed.png'
 
-// // src/pages/DaySelection.tsx
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import './DaySelection.css';
-// import logo from "../image/upscalemedia-transformed.png"; // Import the logo image
-// import LinhaS from '../components/Linha';
-// import Linha from '../image/n5M3kt01.svg';
-// const days = [
-//   { id: 'segunda-feira', label: 'Seg' },
-//   { id: 'terça-feira', label: 'Ter' },
-//   { id: 'quarta-feira', label: 'Qua' },
-//   { id: 'quinta-feira', label: 'Qui' },
-//   { id: 'sexta-feira', label: 'Sex' },
-//   { id: 'sábado', label: 'Sáb ' },
-//   { id: 'domingo', label: 'Dom' },
+function DaySelection() {
+  const navigate = useNavigate();
+  return (
+    <section>
+      <div className="pagina-inicial">
 
-// ];
+        <div className='logo'>
+          <img src={Logo} alt="logo" />
+        </div>
 
-// export default function DaySelection() {
-//   const navigate = useNavigate();
+        <div className='escrever'>
+          <div className='email'>
+            <label htmlFor="login" className="texto-email">
+              E-mail
+            </label>
+            <input type="text" />
+          </div>
 
-//   return (
-//     <div className="day-container">
-//         {/* <div>
-//           <img src={logo} alt="" />
-//         </div> */}
-//       <h1>Feedback do dia:</h1>
-//       <LinhaS />
-//       <div className="day-path">
-//         {days.map((d, idx) => (
-//           <React.Fragment key={d.id}>
-//             <div
-//               className="day-circle"
-//               onClick={() => navigate('/select-employee', { state: { dayId: d.id } })}
-//             >
-//               {d.label}
-//             </div>
-//             {}
-//             {idx < days.length - 1 && <div className="day-line" />}
-//           </React.Fragment>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
+          <div className="senha">
+            <label htmlFor="password" className="texto-senha">
+              Senha
+            </label>
+            <input type="text" />
+          </div>
+        </div>
+
+        <div>
+          <button onClick={() => navigate("/")} className="botão" type="button">
+            Entrar
+          </button>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default DaySelection;
