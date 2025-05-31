cbb3b8218e5e9b4f5315b6f5c5cd6c122c488f1a
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-05-31 14:35:58 -0300

    a

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -16,14 +16,14 @@ function Profile() {
                 </div>
             </div>
             <div className='informations'>
-                <div className='info'>Colaboração: 4</div>
-                <div className='info'>Colaboração: 4</div>
-                <div className='info'>Colaboração: 4</div>
-                <div className='info'>Colaboração: 4</div>
-                <div className='info'>Colaboração: 4</div>
-                <div className='info'>Colaboração: 4</div>
-                <div className='info'>Colaboração: 4</div>
-                <div className='info'>Colaboração: 4</div>
+                <div className='info'>Proatividade: 4.2</div>
+                <div className='info'>Competência: 4.8</div>
+                <div className='info'>Comunicação: 3.7</div>
+                <div className='info'>Organização: 4.0</div>
+                <div className='info'>Trabalho em equipe: 4.5</div>
+                <div className='info'>Inovação: 2.3</div>
+                <div className='info'>Pontualidade: 5.0</div>
+                <div className='info'>Resolução de problemas: 3.9</div>
             </div>
             
         </div>
